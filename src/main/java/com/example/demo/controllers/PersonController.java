afbd0c6aac2bcfc27c2de1f78a5ed684ac95adab
Kristijan <kristijan.pajtasev@hotmail.com>
2021-01-02 23:13:44 +0000

    person controller

--- src/main/java/com/example/demo/controllers/PersonController.java
+++ src/main/java/com/example/demo/controllers/PersonController.java
@@ -1,19 +1,18 @@
 package com.example.demo.controllers;
 
-import com.example.demo.models.Greeting;
+import com.example.demo.models.Person;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Date;
 
 @RestController
 public class PersonController {
 
 
     @GetMapping("/person")
-    public Greeting person() {
-        return null;
+    public Person person() {
+        return new Person("John", "Doe", new Date());
 //        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }

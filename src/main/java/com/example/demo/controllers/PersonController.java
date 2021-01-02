e65219a1e6e6f92e75b589123240ce382519785b
Kristijan <kristijan.pajtasev@hotmail.com>
2021-01-02 23:16:07 +0000

    return list of people

--- src/main/java/com/example/demo/controllers/PersonController.java
+++ src/main/java/com/example/demo/controllers/PersonController.java
@@ -4,15 +4,19 @@ import com.example.demo.models.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class PersonController {
 
 
     @GetMapping("/person")
-    public Person person() {
-        return new Person("John", "Doe", new Date());
+    public List<Person> person() {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("John", "Doe", new Date()));
+        return people;
 //        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }

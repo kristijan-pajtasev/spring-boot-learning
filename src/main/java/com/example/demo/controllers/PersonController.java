28c72963e878cf6a2c56f362fb22ffd3aaa10265
Kristijan <kristijan.pajtasev@hotmail.com>
2021-01-03 01:21:06 +0000

    throw error for not found

--- src/main/java/com/example/demo/controllers/PersonController.java
+++ src/main/java/com/example/demo/controllers/PersonController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +13,22 @@ import java.util.List;
 
 @RestController
 public class PersonController {
+    List<Person> people = new ArrayList<>();
 
+    {
+        people.add(new Person(1, "John", "Doe", new Date()));
+    }
 
     @GetMapping("/person")
     public List<Person> person() {
-        List<Person> people = new ArrayList<>();
-        people.add(new Person(1, "John", "Doe", new Date()));
         return people;
     }
 
     @GetMapping("/person/{id}")
     public Person person(@PathVariable("id") int id) {
-        return new Person(id, "John", "Doe", new Date());
+        for(Person person : people) {
+            if(person.getId() == id) return person;
+        }
+        throw new Error();
     }
 }

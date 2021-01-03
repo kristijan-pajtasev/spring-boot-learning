78624e3f979fb50dc04c33a5612e1f6c352a8fbf
Kristijan <kristijan.pajtasev@hotmail.com>
2021-01-03 02:02:01 +0000

    save person to list

--- src/main/java/com/example/demo/controllers/PersonController.java
+++ src/main/java/com/example/demo/controllers/PersonController.java
@@ -3,10 +3,10 @@ package com.example.demo.controllers;
 import com.example.demo.models.Person;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 public class PersonController {
     List<Person> people = new ArrayList<>();
+    private int id = 2;
 
     {
         people.add(new Person(1, "John", "Doe", new Date()));
@@ -27,9 +28,18 @@ public class PersonController {
 
     @GetMapping("/person/{id}")
     public Person person(@PathVariable("id") int id) {
-        for(Person person : people) {
-            if(person.getId() == id) return person;
+        for (Person person : people) {
+            if (person.getId() == id) return person;
         }
         throw new Error();
+    }
+
+    @PostMapping("/person")
+    public Person makePerson(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String dob) throws ParseException {
+        id++;
+        Date dateOfBirth = new SimpleDateFormat("yyyy-MM-dd").parse(dob);
+        Person person = new Person(id, firstName, lastName, dateOfBirth);
+        people.add(person);
+        return person;
     }
 }
